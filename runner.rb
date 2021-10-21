053adedfd1c677a865c8b2f9bce8dcf99afbd6c0
SKushnir <SKushnir@i.ua>
2021-10-21 20:31:56 +0300

    initial

---
+++ runner.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'miniql'
+
+require_relative 'config'
+require_relative 'game'
+require_relative 'question'
+require_relative 'text'
+require_relative 'answer_button'
+require_relative 'main'
+
+Millionaire::Game.new
